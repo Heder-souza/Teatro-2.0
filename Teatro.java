227c288fa7f1aabadf9ae69084a05a6cad1fd4c5
Heder-souza <hederdesouzasilva@gmail.com>
2026-08-08 13:53:37 -0300

    visualizar teatro concluido

--- Teatro.java
+++ Teatro.java
@@ -5,6 +5,8 @@ public class Teatro {
         Scanner scanner = new Scanner(System.in);
 
         int[][] assentos = new int[20][20];
+        char[] letras = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'};
+
         int escolha = 0;
 
         do {
@@ -16,7 +18,7 @@ public class Teatro {
 
             switch (escolha) {
                 case 1:
-                    visualizar_teatro(assentos);
+                    visualizar_teatro(assentos, letras);
                     break;
 
                 case 2:
@@ -32,7 +34,7 @@ public class Teatro {
                     break;
 
                 case 5:
-                    
+
                     break;
 
                 case 0:
@@ -48,16 +50,30 @@ public class Teatro {
         scanner.close();
     }
 
-    public static void visualizar_teatro(int[][] assentos) {
+    
+    public static void visualizar_teatro(int[][] assentos, char[] letras) {
         System.out.println("\nBem vindo ao Teatro Devisate!!\n\nL - Livre | V - Vendido | R - Reservado\n");
+
+        System.out.print("   ");
+        for (int colunas = 0; colunas <= 19; colunas++) {
+                        if (colunas < 10) {
+                System.out.print(colunas+1 + "    "); 
+            } else {
+                System.out.print(colunas+1 + "   ");
+            }
+        
+        }
+        System.out.println();
         for (int i = 0; i < assentos.length; i++) {
+            System.out.print(letras[i]);
             for (int j = 0; j < assentos.length; j++) {
+
                 if (assentos[i][j] == 0) {
-                    System.out.print("[L]" + " ");
+                    System.out.print("  [L]");
                 } else if (assentos[i][j] == 1) {
-                    System.out.print("[R]" + " ");
+                    System.out.print("  [R]");
                 } else {
-                    System.out.print("[V]" + " ");
+                    System.out.print("  [V]");
                 }
 
             }
